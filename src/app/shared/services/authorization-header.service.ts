11c926804daa7d441c5afcaff601fa24eff7d322
Lee Hodges <leehodges@gmail.com>
2020-09-15 10:47:14 -0500

    Debug Signup
    authorization-header.service.ts
    const user was not set to get currentUser
    const authReq when calling BearerToken was using '' instead of ``
    You also did not have your else return statement set
    
    user.service.ts
    Lots of instances of using '' instead of `` when calling API url
    `` are called Template Literals and more information can be found here for when to use https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
    
    Basically use anytime you are calling something wtih ${}

--- src/app/shared/services/authorization-header.service.ts
+++ src/app/shared/services/authorization-header.service.ts
@@ -4,39 +4,35 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgbPaginationNext } from '@ng-bootstrap/ng-bootstrap';
 import { onErrorResumeNext } from 'rxjs/operators';
-
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorizationHeaderService implements HttpInterceptor {
-
   constructor(
     private router: Router,
     private storage: LocalStorageService
   ) { }
-
   intercept( req: HttpRequest<any>, next: HttpHandler) {
-      const blackListedRoutes = [
-        '/users/login'
-      ]
-        const token = this.storage.getItem('accessToken') // grabs the token from local storage
-
-        const user = this.storage.getItem
-
-        let found = false
-
-        for(let i = 0; i < blackListedRoutes.length; i ++) {
-          if (blackListedRoutes[i] === req['url']) {
-              found = true;
-              break;
-          }
-        }
-
-
-        if ( user && token && found === false) {
-          const authReq = req.clone({setHeaders: { 'Authorization': 'Bearer ${token}'}})
-          return next.handle(authReq)
-        }
-
+    const blackListedRoutes = [
+      '/users/login'
+    ]
+    const token = this.storage.getItem('accessToken') // grabs the token from local storage
+    //You did not have the constuser get Item defined as currentUser
+    const user = this.storage.getItem('currentUser')
+    let found = false
+    for(let i = 0; i < blackListedRoutes.length; i ++) {
+      if (blackListedRoutes[i] === req['url']) {
+        found = true;
+        break;
       }
     }
+    // Need `` not '' on Bearer token
+    if ( user && token && found === false) {
+      const authReq = req.clone({setHeaders: { 'Authorization': `Bearer ${token}`}})
+      return next.handle(authReq)
+    //  You did not have the else
+    } else {
+      return next.handle(req)
+    }
+  }
+}
--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -3,7 +3,7 @@ import { User } from './models/user';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment} from './../../../environments/environment.prod';
-  import { BehaviorSubject, from, Observable, throwError } from 'rxjs';
+import { BehaviorSubject, from, Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { faFileWord } from '@fortawesome/free-solid-svg-icons';
 import { Router } from '@angular/router';
@@ -12,100 +12,104 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class UserService {
-  private currentUserSubject: BehaviorSubject<User>
-  public currentUser: Observable<User>
-  private userApi: string
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
+  private userApi: string;
   constructor(
     private router: Router,
     private http: HttpClient,
     private storage: LocalStorageService
     ){
-      this.userApi = '${environment.apiUrl}api/v1/users'
-      this.currentUserSubject = new BehaviorSubject<User>(this.storage.getItem('currentUser'))
-      this.currentUser = this.currentUserSubject.asObservable()
+      // `${environment.apiUrl}api/v1/users` needs to be `` not ''
+      this.userApi = `${environment.apiUrl}api/v1/users`;
+      this.currentUserSubject = new BehaviorSubject<User>(this.storage.getItem('currentUser'));
+      this.currentUser = this.currentUserSubject.asObservable();
       }
 
     public get currentUserValue(): User {
-      return this.currentUserSubject.value // returns the currentUser value to a component
+      return this.currentUserSubject.value; // returns the currentUser value to a component
     }
 
     setCurrentUser(user: User){
-      this.currentUserSubject.next(user) //sets the currentUserSubject
+      this.currentUserSubject.next(user); // sets the currentUserSubject
     }
 
 
   login(params) {
-    return this.http.post<any>('${this.userApi}/login', params)
+    // needs to be `` not ''
+    return this.http.post<any>(`${this.userApi}/login`, params)
     .pipe(
       catchError(this.handleError),
       map(res => {
         if (res && res.token) {
-          const newUser = new User (res)
-          this.storage.setItem('accessTokeen', res.token)
-          this.storage.setItem('currentUser', newUser)
-          this.currentUserSubject.next(newUser)
-          return { success: true, user: newUser }
+          const newUser = new User (res);
+          this.storage.setItem('accessTokeen', res.token);
+          this.storage.setItem('currentUser', newUser);
+          this.currentUserSubject.next(newUser);
+          return { success: true, user: newUser };
         } else {
-          return { success: false, msg: 'Invalid Credentials!'}
+          return { success: false, msg: 'Invalid Credentials!'};
         }
       })
-    )
+    );
   }
 
   signup(params) {
-    return this.http.post<any>('${this.userApi}/create', params)
+    // needs to be `` not ''
+    return this.http.post<any>(`${this.userApi}/create`, params)
     .pipe(
       catchError(this.handleError),
       map(res => {
-        debugger
+        debugger;
         if (res && res.token) {
-          const newUser = new User (res)
-          this.storage.setItem('accessTokeen', res.token)
-          this.storage.setItem('currentUser', newUser)
-          this.currentUserSubject.next(newUser)
-          return { success: true, user: newUser }
+          const newUser = new User (res);
+          this.storage.setItem('accessTokeen', res.token);
+          this.storage.setItem('currentUser', newUser);
+          this.currentUserSubject.next(newUser);
+          return { success: true, user: newUser };
         }
       })
-    )
+    );
   }
 
 logoutUser() {
   this.logout().subscribe( data => {
-    //logout was successful
+    // logout was successful
     if (data) {
-      this.removeCurrentUserAndRoute()
+      this.removeCurrentUserAndRoute();
     }
   }, error => {
     if (error) {
-      this.removeCurrentUserAndRoute()
+      this.removeCurrentUserAndRoute();
     }
-  })
+  });
 }
 
   logout() {
-    return this.http.delete<any>('${this.userApi}/logout', {})
+    // needs to be `` not ''
+    return this.http.delete<any>(`${this.userApi}/logout`, {});
   }
 
   removeCurrentUserAndRoute() {
     // set local storage vars as undefined, remove, and then route back
-    this.storage.setItem('currentUser', undefined)
-    this.storage.setItem('accessToken', undefined)
-    this.currentUserSubject.next(null)
-    this.storage.removeItem('currentUser')
-    this.storage.removeItem('accessToken')
-    this.router.navigate(['/login'])
+    this.storage.setItem('currentUser', undefined);
+    this.storage.setItem('accessToken', undefined);
+    this.currentUserSubject.next(null);
+    this.storage.removeItem('currentUser');
+    this.storage.removeItem('accessToken');
+    this.router.navigate(['/login']);
   }
 
   handleError(error) {
-     let returnError
+     let returnError;
      if (error.error instanceof ErrorEvent) {
-       // client-side error
-       returnError = { statusCode: error.error.statusCode, message: 'Error ${error.error.message}'}
+       // client-side error needs to be `` not ''
+       returnError = { statusCode: error.error.statusCode, message: `Error ${error.error.message}`};
       }
       else {
-      // servier-side error
-      returnError = { statusCode: error.error.statusCode, message: 'Error Code: ${error.status}\nMessage: ${error.message}'}
+      // servier-side error needs to be `` not ''
+      returnError = { statusCode: error.error.statusCode, message: `Error Code: ${error.status}\nMessage: ${error.message}`};
       }
-      return throwError(returnError)
+     return throwError(returnError);
    }
   }
