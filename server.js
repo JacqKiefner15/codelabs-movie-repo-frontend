a572deadfce907f6e2805af93504c10cb98ab5f7
JacqKiefner15 <jacqkiefner@gmail.com>
2020-10-19 20:24:52 -0500

    debug server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(forceSsl);
 app.use(express.static('./dist/codelabs-movie-frontend'));
 
 app.get('/*', (req,res)=>{
-  res.sendFile(path.join(_dirname, '.dist/codelabs-movie-frontend'))
+  res.sendFile(path.join(_dirname, '/dist/codelabs-movie-frontend'))
 });
 
 app.listen(process.env.PORT || 8080, ()=>{
