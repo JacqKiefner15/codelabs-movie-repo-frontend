d4987b82575fa992975057db9320aae39c90f495
JacqKiefner15 <jacqkiefner@gmail.com>
2020-10-20 10:38:43 -0500

    server.js path change

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/codelabs-movie-frontend'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/codelabs-movie-frontend/index.html'));});
+'/dist/codelabs-movie-frontend.html'));});
 app.listen(process.env.PORT || 8080);
