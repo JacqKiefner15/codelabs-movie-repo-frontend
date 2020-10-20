a1971e91daa2246caa27ab746bbeb59e86ca1603
JacqKiefner15 <jacqkiefner@gmail.com>
2020-10-20 10:35:38 -0500

    updates for heroku push

--- server.js
+++ server.js
@@ -1,22 +1,8 @@
-const express = require('express',)
-var forceSsl = require('force-ssl-heroku');
-const path = require('path')
-const enableProdMode = require('@angular/core').enableProdMode
-
-enableProdMode();
-
+const express = require('express');
+const path = require('path');
 const app = express();
-
-app.use(forceSsl);
-
-app.use(express.static('./dist/codelabs-movie-frontend'));
-
-app.get('/*', (req,res)=>{
-  res.sendFile(path.join(_dirname, '/dist/codelabs-movie-frontend'))
-});
-
-app.listen(process.env.PORT || 8080, ()=>{
-  console.log('Server started')
-})
-
-
+app.use(express.static(__dirname + '/dist/codelabs-movie-frontend'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/codelabs-movie-frontend/index.html'));});
+app.listen(process.env.PORT || 8080);
